457cd9d40f5f1d7d172ba31e9ebdd1b87609b4b5
Nguyen Do <konu0315@gmail.com>
2026-03-06 09:35:54 -0500

    Refactor biladlyklabord.go: reorganize imports and improve check function for clarity

--- kattis-solutions/biladlyklabord/biladlyklabord.go
+++ kattis-solutions/biladlyklabord/biladlyklabord.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+func check (inp string) string{
+	if len(inp) < 2{
+		return inp
+	}
+	r_inp := []rune(inp)
+	var outp []rune
+	old := r_inp[0]
+	for i:=1;i<len(inp);i++ {
+		if r_inp[i]!=old{
+			outp = append(outp, old)
+			old = r_inp[i]
+		} else {
+			if i==len(inp)-1{
+				outp = append(outp, r_inp[i])
+			}
+		}
+	}
+	return string(outp)
+}
 func main(){
-	var 
+	in := bufio.NewReader(os.Stdin)
+	inp,_ := in.ReadString('\n')
+	fmt.Print(check(inp))
 }
